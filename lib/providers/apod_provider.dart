00444f88ed8347ed20ddd8cb69b5d3b9dd78fd91
Kavienan J <kavienanj@gmail.com>
2020-10-14 10:39:40 +0530

    Added instant caching of api calls

--- lib/providers/apod_provider.dart
+++ lib/providers/apod_provider.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
-
+import 'package:hive/hive.dart';
 import '../api_key.dart';
 
 class ApodProvider extends ChangeNotifier {
@@ -17,15 +17,20 @@ class ApodProvider extends ChangeNotifier {
   get month => date.month;
   get day => date.day;
 
-  Future<Response> getApodData() {
+  String getUrl() {
     String dateString = date.year.toString() +
         "-" +
         date.month.toString() +
         "-" +
         date.day.toString();
-    String url = Uri.encodeFull(
-        "https://api.nasa.gov/planetary/apod?date=$dateString&hd=true&api_key=$apiKey");
-    Future<Response> response = get(url);
-    return response;
+    return Uri.encodeFull("https://api.nasa.gov/planetary/apod?date=$dateString&hd=true&api_key=$apiKey");
+  }
+
+  Future<String> getApodData() async {
+    String url = getUrl();
+    Response response = await get(url);
+    Box box = await Hive.openBox("cachedResponses");
+    box.put(url, response.body);
+    return response.body;
   }
 }
